a1095af772c3aed45cb66d918326eb41c417205c
git155371 <15537165136@163.com>
2017-11-29 16:02:12 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -90,8 +90,8 @@ function getFooterMarginTop() {
 function categoryDisplay() {
     /*only show All*/
     $('.post-list-body>div[post-cate!=All]').hide();
-    /*show category when click categories list*/
-    $('.categories-list-item').click(function() {
+    /*show category when click android list*/
+    $('.android-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
 
         $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
